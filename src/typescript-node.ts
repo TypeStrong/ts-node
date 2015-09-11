27f6fef1a7897316c0b515aaabe461e9b67c1071
Blake Embrey <hello@blakeembrey.com>
2015-09-10 20:44:06 -0700

    v0.1.0

--- src/typescript-node.ts
+++ src/typescript-node.ts
@@ -12,7 +12,7 @@ import chalk = require('chalk')
 /**
  * Export the current version.
  */
-export const VERSION = '0.0.9'
+export const VERSION = '0.1.0'
 
 /**
  * Extensions to compile using TypeScript.
