4d9e5434fe7efc9edb694628dcec7ad80863e492
Blake Embrey <hello@blakeembrey.com>
2015-11-01 21:08:18 -0800

    Replace TSD with typings

--- src/typings/make-error.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'make-error' {
-  export class BaseError implements Error {
-    message: string
-    name: string
-    stack: string
-
-    constructor (message: string)
-  }
-}
--- src/typings/pinkie-promise.d.ts
+++
@@ -1,3 +0,0 @@
-declare module "pinkie-promise" {
-  export = Promise
-}
