6fd286fff2f66eba2f9bed1c156703af8dfbd74a
Blake Embrey <hello@blakeembrey.com>
2015-11-16 12:57:11 -0800

    v0.5.2

--- src/ts-node.ts
+++ src/ts-node.ts
@@ -46,7 +46,7 @@ export type TSish = TS_1_5ish | TS_1_7ish
 /**
  * Export the current version.
  */
-export const VERSION = '0.5.1'
+export const VERSION = '0.5.2'
 
 /**
  * Extensions to compile using TypeScript.
