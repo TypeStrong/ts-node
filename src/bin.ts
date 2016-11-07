3c3ebb14848c2f8ee14f692913048535a5a2779f
Blake Embrey <hello@blakeembrey.com>
2016-11-07 11:18:59 -0800

    Refactor `bin` file, use `v8flags`
    Closes #230

--- src/bin.ts
+++ src/bin.ts
@@ -2,64 +2,66 @@
 
 import { spawn } from 'child_process'
 import { join } from 'path'
+import v8flags = require('v8flags')
 
-const args = [join(__dirname, '_bin.js')]
-const opts = process.argv.slice(2)
+const argv = process.argv.slice(2)
 
-for (let i = 0; i < opts.length; i++) {
-  const arg = opts[i]
-  const flag = arg.split('=', 1)[0]
-
-  switch (flag) {
-    case '-d':
-      args.unshift('--debug')
-      opts.splice(i, 1)
-      break
-    case '-gc':
-    case '--expose-gc':
-      args.unshift('--expose-gc')
-      opts.splice(i, 1)
-      break
-    case 'debug':
-    case '--debug':
-    case '--debug-brk':
-    case '--nolazy':
-    case '--inspect':
-    case '--gc-global':
-    case '--es_staging':
-    case '--no-deprecation':
-    case '--prof':
-    case '--log-timer-events':
-    case '--throw-deprecation':
-    case '--trace-deprecation':
-    case '--use_strict':
-    case '--allow-natives-syntax':
-    case '--perf-basic-prof':
-      args.unshift(arg)
-      opts.splice(i, 1)
-      break
-    default:
-      if (/^--(?:harmony|trace|icu-data-dir|max-old-space-size)/.test(arg)) {
-        args.unshift(arg)
-        opts.splice(i, 1)
-      }
-      break
+v8flags(function (err, v8flags) {
+  if (err) {
+    console.error(err.stack)
+    process.exit(1)
+    return
   }
 
-  // Stop on first non-argument because it's the script name.
-  if (/^[^-]/.test(arg)) {
-    break
-  }
-}
+  const nodeArgs: string[] = []
+  const scriptArgs: string[] = []
 
-const proc = spawn(process.execPath, args.concat(opts), { stdio: 'inherit' })
+  const knownFlags = v8flags.concat([
+    'debug',
+    '--debug',
+    '--debug-brk',
+    '--inspect',
+    '--nolazy',
+    '--no-deprecation',
+    '--log-timer-events',
+    '--throw-deprecation',
+    '--trace-deprecation',
+    '--allow-natives-syntax',
+    '--perf-basic-prof'
+  ])
 
-proc.on('exit', function (code: number, signal: string) {
-  process.on('exit', function () {
-    if (signal) {
-      process.kill(process.pid, signal)
+  for (let i = 0; i < argv.length; i++) {
+    const arg = argv[i]
+    const flag = arg.split('=', 1)[0]
+
+    if (flag === '-d') {
+      nodeArgs.push('--debug')
+    } else if (flag === '-gc') {
+      nodeArgs.push('--expose-gc')
+    } else if (knownFlags.indexOf(flag) > -1) {
+      nodeArgs.push(arg)
+    } else if (/^-/.test(flag)) {
+      scriptArgs.push(arg)
     } else {
-      process.exit(code)
+      // Break when we encounter a "script".
+      scriptArgs.push(...argv.slice(i))
+      break
     }
+  }
+
+  const proc = spawn(
+    process.execPath,
+    nodeArgs.concat(join(__dirname, '_bin.js'), scriptArgs),
+    { stdio: 'inherit' }
+  )
+
+  proc.on('exit', function (code: number, signal: string) {
+    process.on('exit', function () {
+      if (signal) {
+        process.kill(process.pid, signal)
+      } else {
+        process.exit(code)
+      }
+    })
   })
 })
