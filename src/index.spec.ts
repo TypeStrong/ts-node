b251147375a242bacc6de146ea9fe6445fcd532a
Blake Embrey <hello@blakeembrey.com>
2017-03-20 18:15:24 -0700

    Make tests work under Windows (avoid `|` in shell)

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -135,30 +135,36 @@ describe('ts-node', function () {
     })
 
     it('should pipe into `ts-node` and evaluate', function (done) {
-      exec(`echo "console.log('hello')" | ${BIN_EXEC}`, function (err, stdout) {
+      const cp = exec(BIN_EXEC, function (err, stdout) {
         expect(err).to.not.exist
         expect(stdout).to.equal('hello\n')
 
         return done()
       })
+
+      cp.stdin.end("console.log('hello')")
     })
 
     it('should pipe into `ts-node`', function (done) {
-      exec(`echo "true" | ${BIN_EXEC} -p`, function (err, stdout) {
+      const cp = exec(`${BIN_EXEC} -p`, function (err, stdout) {
         expect(err).to.not.exist
         expect(stdout).to.equal('true\n')
 
         return done()
       })
+
+      cp.stdin.end('true')
     })
 
     it('should pipe into an eval script', function (done) {
-      exec(`echo "true" | ${BIN_EXEC} -p '(process.stdin as any).isTTY'`, function (err, stdout) {
+      const cp = exec(`${BIN_EXEC} -p '(process.stdin as any).isTTY'`, function (err, stdout) {
         expect(err).to.not.exist
         expect(stdout).to.equal('undefined\n')
 
         return done()
       })
+
+      cp.stdin.end('true')
     })
 
     it('should support require flags', function (done) {
