3e9d7326278e94910e69dbc227f7a31853a5b6f2
Otsuki Hitoshi <hitochan777@gmail.com>
2018-01-25 01:13:13 +0900

    Fix bug of TS_NODE_TYPE_CHECK and TS_NODE_CACHE not taking effect

--- src/index.ts
+++ src/index.ts
@@ -169,8 +169,8 @@ export function register (options: Options = {}): Register {
   ).concat(emptyFileListWarnings).map(Number)
   const getFile = options.getFile || DEFAULTS.getFile
   const fileExists = options.fileExists || DEFAULTS.fileExists
-  const shouldCache = !!(options.cache === undefined ? DEFAULTS.cache : options.cache)
-  const typeCheck = !!(options.typeCheck === undefined ? DEFAULTS.typeCheck : options.typeCheck)
+  const shouldCache = !!(!options.cache ? DEFAULTS.cache : options.cache)
+  const typeCheck = !!(!options.typeCheck ? DEFAULTS.typeCheck : options.typeCheck)
   const project = options.project === undefined ? DEFAULTS.project : options.project
   const cacheDirectory = options.cacheDirectory || DEFAULTS.cacheDirectory || getTmpDir()
   const compilerOptions = Object.assign({}, DEFAULTS.compilerOptions, options.compilerOptions)
