cac5c2cf5782ba4bb02cc8836b7f01ebbe05067b
tudou527 <xiaoyun.fuxy@alibaba-inc.com>
2018-12-18 16:31:48 +0800

    test: test file path

--- src/index.ts
+++ src/index.ts
@@ -389,6 +389,7 @@ export function parseTransformers (files: string | string[], cwd: string): ts.Cu
   const transformers = {} as ts.CustomTransformers;
 
   ([] as string[]).concat(files).map((file: string) => {
+    console.log('>>>>> file: ', file, cwd);
     const filePath = require.resolve(file.trim(), { paths: [ cwd ] })
     const trans = require(filePath) as ts.CustomTransformers
 
