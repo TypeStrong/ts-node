e87dfa1c14fdd7e497bb01d5c9a598236371bbeb
Andrew Bradley <cspotcode@gmail.com>
2019-12-14 02:43:15 -0500

    More removal of '--require' support

--- tests/tsconfig-options/log-options.js
+++ tests/tsconfig-options/log-options.js
@@ -1,5 +1,4 @@
 const assert = require('assert')
-assert(process.required)
 const register = process[Symbol.for('ts-node.register.instance')]
 console.log(JSON.stringify({
   options: register.options,
