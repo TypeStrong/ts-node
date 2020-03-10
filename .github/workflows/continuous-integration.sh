51d0d62566df1a00818f67fbc1eaccc425032d3f
Andrew Bradley <cspotcode@gmail.com>
2020-03-09 22:11:50 -0400

    Upload coverage from Github Actions

---
+++ .github/workflows/continuous-integration.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+npm install
+npm run build
+npm rm tslint
+npm install "$matrix_typescript"
+
+# Test and capture exit code
+set +e
+npm run test-cov
+exit_code="$?"
+set -e
+
+# Report coverage
+npm install coveralls@2
+cat ./coverage/lcov.info | coveralls
+
+exit "$exit_code"
