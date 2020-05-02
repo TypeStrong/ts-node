023c5d12c4edf1c5a398f93e340c021da6a96f96
Andrew Bradley <cspotcode@gmail.com>
2020-05-02 19:58:02 -0400

    type annotation in esm.mjs

--- esm.mjs
+++ esm.mjs
@@ -2,4 +2,6 @@ import {fileURLToPath} from 'url'
 import {createRequire} from 'module'
 const require = createRequire(fileURLToPath(import.meta.url))
 
-export const {resolve, getFormat, transformSource} = require('./dist/esm').registerAndCreateEsmHooks()
+/** @type {import('./dist/esm')} */
+const esm = require('./dist/esm')
+export const {resolve, getFormat, transformSource} = esm.registerAndCreateEsmHooks()
