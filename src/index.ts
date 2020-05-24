a6aa25b5b3af5dab0ae9b9ee15dc230c18c6a79d
Harminder virk <virk.officials@gmail.com>
2020-05-24 14:40:16 +0530

    refactor: move typeof factory check to its original position

--- src/index.ts
+++ src/index.ts
@@ -488,15 +488,11 @@ export function create (rawOptions: CreateOptions = {}): Register {
   let getTypeInfo: (_code: string, _fileName: string, _position: number) => TypeInfo
 
   const getOutputTranspileOnly = (code: string, fileName: string, overrideCompilerOptions?: Partial<_ts.CompilerOptions>): SourceOutput => {
-    if (typeof transformers === 'function') {
-      throw new TypeError('Transformers function is unavailable in "--transpile-only"')
-    }
-
     const result = ts.transpileModule(code, {
       fileName,
       compilerOptions: overrideCompilerOptions ? { ...config.options, ...overrideCompilerOptions } : config.options,
       reportDiagnostics: true,
-      transformers: transformers
+      transformers: transformers as Exclude<typeof transformers, Function>
     })
 
     const diagnosticList = filterDiagnostics(result.diagnostics || [], ignoreDiagnostics)
@@ -790,6 +786,10 @@ export function create (rawOptions: CreateOptions = {}): Register {
       }
     }
   } else {
+    if (typeof transformers === 'function') {
+      throw new TypeError('Transformers function is unavailable in "--transpile-only"')
+    }
+
     getOutput = getOutputTranspileOnly
 
     getTypeInfo = () => {
