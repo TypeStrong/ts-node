04e789693768dad32c3cd3396fb2a6077b4a76db
Andrew Bradley <cspotcode@gmail.com>
2020-07-29 03:07:32 -0400

    enable test for react tsx sourcemaps (#1088)

--- tests/throw-react-tsx.tsx
+++ tests/throw-react-tsx.tsx
@@ -1,7 +1,7 @@
 // intentional whitespace to prove that sourcemaps are working.  Throw should happen on line 100.
 // 100 lines is meant to be far more space than the helper functions would take.
+const React = {createElement: (...args: any[]) => null}
 class Foo {
-
 
 
 
@@ -98,6 +98,9 @@ class Foo {
 
   constructor () { this.bar() }
   bar () { throw new Error('this is a demo') }
+  someJsx() {
+    return <div />
+  }
 }
 new Foo()
 export {}
