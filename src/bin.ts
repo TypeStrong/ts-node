41a0392f882877353af8117e4c0368e3a92e68ba
Andrew Bradley <cspotcode@gmail.com>
2020-07-29 04:02:00 -0400

    Update bin.ts

--- src/bin.ts
+++ src/bin.ts
@@ -365,7 +365,7 @@ function startRepl (service: Register, state: EvalState, code?: string) {
     prompt: '> ',
     input: process.stdin,
     output: process.stdout,
-    terminal: process.stdout.isTTY && !parseInt(process.env.NODE_NO_READLINE!), // tslint:disable-line
+    terminal: process.stdout.isTTY && !parseInt(process.env.NODE_NO_READLINE!, 10),
     eval: replEval,
     useGlobal: true
   })
