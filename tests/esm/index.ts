8809b7e33c81d302fb737b65b00977a481d29fe3
Andrew Bradley <cspotcode@gmail.com>
2020-10-31 16:34:12 -0400

    Add ESM test of builtin module resolution

--- tests/esm/index.ts
+++ tests/esm/index.ts
@@ -3,6 +3,10 @@ import {bar} from './bar.js'
 import {baz} from './baz.js'
 import {biff} from './biff.js'
 
+// Test import builtin modules
+import {readFileSync} from 'fs';
+if(typeof readFileSync !== 'function') throw new Error('failed to import builtin module')
+
 if(typeof module !== 'undefined') throw new Error('module should not exist in ESM')
 
 console.log(`${foo} ${bar} ${baz} ${biff}`)
