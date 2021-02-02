38559f3dc549990e24f5fba0fbaf83d1532ba096
Andrew Bradley <cspotcode@gmail.com>
2021-02-02 14:07:57 -0500

    homepage code cleanup

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -6,49 +6,6 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
 
-const features = [
-  {
-    // title: 'Easy to Use',
-    imageUrl: 'screenshot.png',
-    // description: (
-    //   <>
-    //     Docusaurus was designed from the ground up to be easily installed and
-    //     used to get your website up and running quickly.
-    //   </>
-    // ),
-  },
-  // {
-  //   title: 'Easy to Use',
-  //   imageUrl: 'img/undraw_docusaurus_mountain.svg',
-  //   description: (
-  //     <>
-  //       Docusaurus was designed from the ground up to be easily installed and
-  //       used to get your website up and running quickly.
-  //     </>
-  //   ),
-  // },
-  // {
-  //   title: 'Focus on What Matters',
-  //   imageUrl: 'img/undraw_docusaurus_tree.svg',
-  //   description: (
-  //     <>
-  //       Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-  //       ahead and move your docs into the <code>docs</code> directory.
-  //     </>
-  //   ),
-  // },
-  // {
-  //   title: 'Powered by React',
-  //   imageUrl: 'img/undraw_docusaurus_react.svg',
-  //   description: (
-  //     <>
-  //       Extend or customize your website layout by reusing React. Docusaurus can
-  //       be extended while reusing the same header and footer.
-  //     </>
-  //   ),
-  // },
-];
-
 function Feature({imageUrl, title, description}) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
@@ -58,8 +15,6 @@ function Feature({imageUrl, title, description}) {
           <img className={styles.featureImage} src={imgUrl} alt={title} />
         </div>
       )}
-      {/* <h3>{title}</h3> */}
-      {/* <p>{description}</p> */}
     </div>
   );
 }
@@ -90,27 +45,11 @@ function Home() {
       <main>
         <section className={styles.features}>
           <div className="container">
-            {/* <div className="row">
-              <div className={clsx('col col--4', styles.feature)}> */}
                 <div className="text--center">
-                    {/* <img className={styles.featureImage} src="https://github.com/TypeStrong/ts-node/raw/master/screenshot.png" alt="screenshot of ts-node" /> */}
                     <img src={useBaseUrl('img/screenshot.png')} alt="screenshot of ts-node" />
                 </div>
-              {/* </div>
-            </div> */}
           </div>
         </section>
-        {/* {features && features.length > 0 && (
-          <section className={styles.features}>
-            <div className="container">
-              <div className="row">
-                {features.map((props, idx) => (
-                  <Feature key={idx} {...props} />
-                ))}
-              </div>
-            </div>
-          </section>
-        )} */}
       </main>
     </Layout>
   );
