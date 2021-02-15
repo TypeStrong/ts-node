9734642849a4329798677808201d350d3bb70c73
Andrew Bradley <cspotcode@gmail.com>
2021-02-15 05:00:04 -0500

    fix tests to avoid type error on ts2.7

--- tests/cwd-and-script-mode/a/index.ts
+++ tests/cwd-and-script-mode/a/index.ts
@@ -1,5 +1,6 @@
 export {}
-const register = process[Symbol.for('ts-node.register.instance')]
+// Type assertion to please TS 2.7
+const register = process[(Symbol as any).for('ts-node.register.instance')]
 console.log(JSON.stringify({
   options: register.options,
   config: register.config
