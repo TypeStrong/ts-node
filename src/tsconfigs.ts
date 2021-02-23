42b638438b435f17a6429777f148e19f95663202
Andrew Bradley <cspotcode@gmail.com>
2021-02-23 14:57:13 -0500

    lint fix

--- src/tsconfigs.ts
+++ src/tsconfigs.ts
@@ -4,20 +4,21 @@ const nodeMajor = parseInt(process.versions.node.split('.')[0], 10)
 /**
  * return parsed JSON of the bundled @tsconfig/bases config appropriate for the
  * running version of nodejs
+ * @internal
  */
 export function getDefaultTsconfigJsonForNodeVersion (ts: TSCommon): any {
-  if(nodeMajor >= 14) {
+  if (nodeMajor >= 14) {
     const config = require('@tsconfig/node14/tsconfig.json')
-    if(configCompatible(config)) return config
+    if (configCompatible(config)) return config
   }
-  if(nodeMajor >= 12) {
+  if (nodeMajor >= 12) {
     const config = require('@tsconfig/node12/tsconfig.json')
-    if(configCompatible(config)) return config
+    if (configCompatible(config)) return config
   }
   return require('@tsconfig/node10/tsconfig.json')
 
   // Verify that tsconfig target and lib options are compatible with TypeScript compiler
-  function configCompatible(config: {
+  function configCompatible (config: {
     compilerOptions: {
       lib: string[],
       target: string
