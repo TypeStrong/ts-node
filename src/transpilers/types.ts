7a855ed80adb2660b7060fca1f45e53034702945
Andrew Bradley <cspotcode@gmail.com>
2021-02-23 15:01:49 -0500

    add some jsdoc to new transpiler api surface

--- src/transpilers/types.ts
+++ src/transpilers/types.ts
@@ -1,16 +1,26 @@
 import type * as ts from 'typescript'
 import { Service } from '..'
 
+/**
+ * Third-party transpilers are implemented as a CommonJS module with a
+ * named export "create"
+ */
+export interface TranspilerModule {
+  create: TranspilerFactory
+}
+/**
+ * Called by ts-node to create a custom transpiler.
+ */
+export type TranspilerFactory = (options: CreateTranspilerOptions) => Transpiler
 export interface CreateTranspilerOptions {
   // TODO this is confusing because its only a partial Service.  Rename?
   service: Pick<Service, 'config' | 'options'>
 }
-export type TranspilerFactory = (options: CreateTranspilerOptions) => Transpiler
-export interface TranspileOptions {
-  fileName: string
-}
 export interface Transpiler {
   transpile (input: string, options: TranspileOptions): TranspileOutput
+}
+export interface TranspileOptions {
+  fileName: string
 }
 export interface TranspileOutput {
   outputText: string
