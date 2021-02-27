eba79911cdc28245df1ea9c4f02a07d8b675f759
Andrew Bradley <cspotcode@gmail.com>
2021-02-27 17:01:54 -0500

    fix test failures caused by prettier formatting changes

--- tests/esm/throw.ts
+++ tests/esm/throw.ts
@@ -1,12 +1,103 @@
 // intentional whitespace to prove that sourcemaps are working.  Throw should happen on line 100.
 // 100 lines is meant to be far more space than the helper functions would take.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Foo {
   constructor() {
     this.bar();
   }
-  bar() {
-    throw new Error('this is a demo');
-  }
+  bar() { throw new Error('this is a demo'); }
 }
 new Foo();
 export {};
