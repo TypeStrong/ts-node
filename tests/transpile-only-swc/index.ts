5e51956e0455bbfa6f2ba671236e03b3f0046d3a
Andrew Bradley <cspotcode@gmail.com>
2021-05-25 17:28:03 -0400

    lint-fix

--- tests/transpile-only-swc/index.ts
+++ tests/transpile-only-swc/index.ts
@@ -1,10 +1,9 @@
 // Test for #1343
-const Decorator = function() {}
+const Decorator = function () {};
 @Decorator
 class World {}
 
 // intentional type errors to check transpile-only ESM loader skips type checking
 parseInt(1101, 2);
-const x: number = `Hello ${ World.name }`;
+const x: number = `Hello ${World.name}`;
 console.log(x);
-
