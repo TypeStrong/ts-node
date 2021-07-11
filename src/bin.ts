f81a5ef2f06c9b0067a938f83dbde1a83c97bf20
ejose19 <8742215+ejose19@users.noreply.github.com>
2021-07-11 19:12:05 -0300

    refactor: adjust main call in bin

--- src/bin.ts
+++ src/bin.ts
@@ -486,14 +486,11 @@ async function evalAndExitOnTsError(
 
 if (require.main === module) {
   // Since main is async, this is needed to get proper exit code and stack.
-  // Enabled natively since node v15.
-  // https://nodejs.org/api/cli.html#cli_unhandled_rejections_mode
-  if (Number(process.versions.node.split('.')[0]) < 15) {
-    // https://github.com/nodejs/node/issues/33577#issuecomment-634920456
-    process.on('unhandledRejection', (e) => {
-      throw e;
-    });
-  }
-
-  main();
+  (async function () {
+    try {
+      await main();
+    } catch (error) {
+      process.emit('uncaughtException', error);
+    }
+  })();
 }
