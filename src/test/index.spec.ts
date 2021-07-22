29f103e4a87d5a4572b6e90f90a4fb41a55f0d68
ejose19 <8742215+ejose19@users.noreply.github.com>
2021-07-22 17:59:22 -0300

    Revert "test: fix esm tests assertion"
    This reverts commit e5cc7344066c259c21773f86f4eff970b17332e9.

--- src/test/index.spec.ts
+++ src/test/index.spec.ts
@@ -1850,9 +1850,7 @@ test.suite('ts-node', (test) => {
         expect(err).to.not.equal(null);
         // expect error from node's default resolver
         expect(stderr).to.match(
-          semver.gte(process.version, '16.5.0')
-            ? /Error:.*(?:\n.*){0,1}\n *at defaultResolve/
-            : /Error \[ERR_UNSUPPORTED_ESM_URL_SCHEME\]:.*(?:\n.*){0,1}\n *at defaultResolve/
+          /Error \[ERR_UNSUPPORTED_ESM_URL_SCHEME\]:.*(?:\n.*){0,1}\n *at defaultResolve/
         );
       });
 
