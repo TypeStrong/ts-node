073bbfc15b82c778cbaa31e08c122210f640c9c1
Jonas Kello <jonas.kello@gmail.com>
2021-09-13 00:23:03 +0200

    Update hooks

--- esm.mjs
+++ esm.mjs
@@ -4,8 +4,4 @@ const require = createRequire(fileURLToPath(import.meta.url));
 
 /** @type {import('./dist/esm')} */
 const esm = require('./dist/esm');
-export const {
-  resolve,
-  getFormat,
-  transformSource,
-} = esm.registerAndCreateEsmHooks();
+export const { resolve, load } = esm.registerAndCreateEsmHooks();
