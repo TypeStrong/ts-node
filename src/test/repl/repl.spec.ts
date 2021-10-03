8a5bec820aafc6789d00aaeaac86352b281d6306
Unlocked <10186337+TheUnlocked@users.noreply.github.com>
2021-10-03 18:21:16 -0400

    Fix linting issue

--- src/test/repl/repl.spec.ts
+++ src/test/repl/repl.spec.ts
@@ -292,11 +292,12 @@ test.suite(
 test.suite(
   'REPL inputs are syntactically independent of each other',
   (test) => {
-
     // Serial because it's timing-sensitive
-    test.serial('arithmetic operators are independent of previous values', async (t) => {
-      const { stdout, stderr } = await t.context.executeInRepl(
-        `9
+    test.serial(
+      'arithmetic operators are independent of previous values',
+      async (t) => {
+        const { stdout, stderr } = await t.context.executeInRepl(
+          `9
         + 3
         7
         - 3
@@ -308,56 +309,66 @@ test.suite(
         ** 2\n.break
         console.log('done!')
         `,
-        { registerHooks: true, waitPattern: 'done!\nundefined\n>' }
-      );
-      expect(stdout).not.toContain('12');
-      expect(stdout).not.toContain('4');
-      expect(stdout).not.toContain('21');
-      expect(stdout).not.toContain('50');
-      expect(stdout).not.toContain('25');
-      expect(stdout).toContain('3');
-      expect(stdout).toContain('-3');
-    });
+          { registerHooks: true, waitPattern: 'done!\nundefined\n>' }
+        );
+        expect(stdout).not.toContain('12');
+        expect(stdout).not.toContain('4');
+        expect(stdout).not.toContain('21');
+        expect(stdout).not.toContain('50');
+        expect(stdout).not.toContain('25');
+        expect(stdout).toContain('3');
+        expect(stdout).toContain('-3');
+      }
+    );
 
     // Serial because it's timing-sensitive
-    test.serial('automatically inserted semicolons do not appear in error messages at the end', async (t) => {
-      const { stdout, stderr } = await t.context.executeInRepl(
-        `(
+    test.serial(
+      'automatically inserted semicolons do not appear in error messages at the end',
+      async (t) => {
+        const { stdout, stderr } = await t.context.executeInRepl(
+          `(
           a
           console.log('done!')`,
-        { registerHooks: true, waitPattern: 'done!\nundefined\n>' }
-      );
-      expect(stderr).toContain("error TS1005: ')' expected.");
-      expect(stderr).not.toContain(';');
-    });
+          { registerHooks: true, waitPattern: 'done!\nundefined\n>' }
+        );
+        expect(stderr).toContain("error TS1005: ')' expected.");
+        expect(stderr).not.toContain(';');
+      }
+    );
 
     // Serial because it's timing-sensitive
-    test.serial('automatically inserted semicolons do not appear in error messages at the start', async (t) => {
-      const { stdout, stderr } = await t.context.executeInRepl(
-        `)
+    test.serial(
+      'automatically inserted semicolons do not appear in error messages at the start',
+      async (t) => {
+        const { stdout, stderr } = await t.context.executeInRepl(
+          `)
         console.log('done!')`,
-        { registerHooks: true, waitPattern: 'done!\nundefined\n>' }
-      );
-      expect(stderr).toContain(
-        'error TS1128: Declaration or statement expected.'
-      );
-      expect(stderr).toContain(')');
-      expect(stderr).not.toContain(';');
-    });
+          { registerHooks: true, waitPattern: 'done!\nundefined\n>' }
+        );
+        expect(stderr).toContain(
+          'error TS1128: Declaration or statement expected.'
+        );
+        expect(stderr).toContain(')');
+        expect(stderr).not.toContain(';');
+      }
+    );
 
     // Serial because it's timing-sensitive
-    test.serial('automatically inserted semicolons do not break function calls', async (t) => {
-      const { stdout, stderr } = await t.context.executeInRepl(
-        `function foo(a: number) {
+    test.serial(
+      'automatically inserted semicolons do not break function calls',
+      async (t) => {
+        const { stdout, stderr } = await t.context.executeInRepl(
+          `function foo(a: number) {
           return a + 1;
       }
       foo(
         1
       )`,
-        { registerHooks: true, waitPattern: '2\n>' }
-      );
-      expect(stderr).toBe('');
-      expect(stdout).toContain('2');
-    });
+          { registerHooks: true, waitPattern: '2\n>' }
+        );
+        expect(stderr).toBe('');
+        expect(stdout).toContain('2');
+      }
+    );
   }
 );
