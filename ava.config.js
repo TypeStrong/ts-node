791234401069ddba945c2ccc423a1e0a4a03d75e
Andrew Bradley <abradley@brightcove.com>
2022-01-20 22:37:55 +0000

    Revert "Update ava.config.js (#1582)"
    This reverts commit aff9bb993c82e9e03cfa96d298ecee982ac8948e.

--- ava.config.js
+++ ava.config.js
@@ -3,8 +3,6 @@ export default {
   failWithoutAssertions: false,
   environmentVariables: {
     ts_node_install_lock: `id-${Math.floor(Math.random() * 10e9)}`,
-    // Force jest expect() errors to generate colorized strings, makes output more readable
-    FORCE_COLOR: '3',
   },
   timeout: '300s',
 };
