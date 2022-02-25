5cc5f420e5dadddc8843b8b388a79e562d5b6e0f
Andrew Bradley <cspotcode@gmail.com>
2022-02-25 02:07:12 -0500

    fix

--- src/child/spawn-child.ts
+++ src/child/spawn-child.ts
@@ -9,7 +9,9 @@ const argPrefix = '--brotli-base64-config=';
 /** @internal */
 export function callInChild(state: BootstrapState) {
   if (!versionGteLt(process.version, '12.17.0')) {
-    throw new Error('`ts-node-esm` and `ts-node --esm` require node version 12.17.0 or newer.');
+    throw new Error(
+      '`ts-node-esm` and `ts-node --esm` require node version 12.17.0 or newer.'
+    );
   }
   const child = spawn(
     process.execPath,
