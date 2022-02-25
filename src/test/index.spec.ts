54fdbba9d08d29e8f50223252586557c23634781
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-02-25 22:55:48 +0700

    test: fallback to node_modules

--- src/test/index.spec.ts
+++ src/test/index.spec.ts
@@ -1271,6 +1271,16 @@ test.suite('ts-node', (test) => {
           );
           expect(err).toBe(null);
         });
+
+        test('fallback to node_modules', async () => {
+          const { err } = await exec(
+            `${CMD_ESM_LOADER_WITHOUT_PROJECT} --project="tsconfig-baseurl-star-path.json" node-modules-star-path.ts`,
+            {
+              cwd: join(TEST_DIR, './esm-path-mapping'),
+            }
+          );
+          expect(err).toBe(null);
+        });
       });
       // TODO ensure these tests run even when `--loader` is not supported
       // Do so by moving these test cases elsewhere
---
+++ tests/esm-path-mapping/node-modules-star-path.ts
@@ -0,0 +1,6 @@
+import * as assert from 'assert';
+
+// Fallback to node_modules
+import somelib from 'some-lib';
+
+assert.strictEqual(somelib, 'some-lib');
