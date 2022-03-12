398db86875917466589fa31cc8455fc1e0e82d54
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-03-12 12:14:56 +0700

    add helper execEsm(...)

--- src/test/path-mapping.spec.ts
+++ src/test/path-mapping.spec.ts
@@ -7,11 +7,23 @@ import {
   TEST_DIR,
 } from './helpers';
 import { join } from 'path';
-import { createExec } from './exec-helpers';
+import { createExec, ExecReturn } from './exec-helpers';
+
+interface ExecEsmOpts {
+  cwd?: string;
+  file?: string;
+  project?: string;
+}
 
 const exec = createExec({
   cwd: TEST_DIR,
 });
+
+const execEsm = ({ cwd, file, project }: ExecEsmOpts): ExecReturn =>
+  exec(`${CMD_ESM_LOADER_WITHOUT_PROJECT} ${file ?? 'index.ts'}`, {
+    cwd: join(TEST_DIR, './esm-path-mapping', cwd ?? ''),
+    ...(project ? { env: { ...process.env, TS_NODE_PROJECT: project } } : {}),
+  });
 
 test.suite('path mapping cjs', (test) => {
   test('path mapping', async () => {
@@ -42,144 +54,93 @@ test.suite('path mapping esm', (test) => {
   test.runIf(nodeSupportsEsmHooks);
 
   test('path mapping', async () => {
-    const { err } = await exec(`${CMD_ESM_LOADER_WITHOUT_PROJECT} index.ts`, {
-      cwd: join(TEST_DIR, './esm-path-mapping'),
-    });
+    const { err } = await execEsm({ file: 'index.ts' });
     expect(err).toBe(null);
   });
 
   test('path mapping error candidates', async () => {
-    const { stderr, err } = await exec(
-      `${CMD_ESM_LOADER_WITHOUT_PROJECT} mapped-not-found.ts`,
-      {
-        cwd: join(TEST_DIR, './esm-path-mapping'),
-      }
-    );
+    const { stderr, err } = await execEsm({ file: 'mapped-not-found.ts' });
+
+    // Expect error
     expect(err).toBeTruthy();
     expect(stderr).toMatch(
       `[ERR_MODULE_NOT_FOUND]: Cannot find 'map2/does-not-exist.ts'`
     );
+
     // Expect tried candidates to be listed
     expect(stderr).toMatch(/- file:\/\/.*mapped\/2-does-not-exist.ts/);
     expect(stderr).toMatch(/- file:\/\/.*mapped\/2a-does-not-exist.ts/);
   });
 
   test('baseUrl set and no paths', async () => {
-    const { err } = await exec(
-      `${CMD_ESM_LOADER_WITHOUT_PROJECT} baseurl-no-paths.ts`,
-      {
-        cwd: join(TEST_DIR, './esm-path-mapping'),
-        env: {
-          ...process.env,
-          TS_NODE_PROJECT: `./tsconfig-baseurl-no-paths.json`,
-        },
-      }
-    );
+    const { err } = await execEsm({
+      file: 'baseurl-no-paths.ts',
+      project: './tsconfig-baseurl-no-paths.json',
+    });
     expect(err).toBe(null);
   });
 
   test('baseUrl set and * path', async () => {
-    const { err } = await exec(
-      `${CMD_ESM_LOADER_WITHOUT_PROJECT} baseurl-star-path.ts`,
-      {
-        cwd: join(TEST_DIR, './esm-path-mapping'),
-        env: {
-          ...process.env,
-          TS_NODE_PROJECT: `./tsconfig-baseurl-star-path.json`,
-        },
-      }
-    );
+    const { err } = await execEsm({
+      file: 'baseurl-star-path.ts',
+      project: './tsconfig-baseurl-star-path.json',
+    });
     expect(err).toBe(null);
   });
 
   test('fallback to node_modules', async (t) => {
-    const { stdout, stderr, err } = await exec(
-      `${CMD_ESM_LOADER_WITHOUT_PROJECT} node-modules-star-path.ts`,
-      {
-        cwd: join(TEST_DIR, './esm-path-mapping'),
-        env: {
-          ...process.env,
-          TS_NODE_PROJECT: `./tsconfig-baseurl-star-path.json`,
-        },
-      }
-    );
-    t.log({ stdout, stderr });
+    const { err } = await execEsm({
+      file: 'node-modules-star-path.ts',
+      project: './tsconfig-baseurl-star-path.json',
+    });
     expect(err).toBe(null);
   });
 
   test('fallback to Node built-in', async () => {
-    const { err } = await exec(
-      `${CMD_ESM_LOADER_WITHOUT_PROJECT} built-in-star-path.ts`,
-      {
-        cwd: join(TEST_DIR, './esm-path-mapping'),
-        env: {
-          ...process.env,
-          TS_NODE_PROJECT: `./tsconfig-baseurl-star-path.json`,
-        },
-      }
-    );
+    const { err } = await execEsm({
+      file: 'built-in-star-path.ts',
+      project: './tsconfig-baseurl-star-path.json',
+    });
     expect(err).toBe(null);
   });
 
   test('skip type definitions', async () => {
-    const { err } = await exec(
-      `${CMD_ESM_LOADER_WITHOUT_PROJECT} type-definition.ts`,
-      {
-        cwd: join(TEST_DIR, './esm-path-mapping'),
-        env: {
-          ...process.env,
-          TS_NODE_PROJECT: `./tsconfig-baseurl-no-paths.json`,
-        },
-      }
-    );
+    const { err } = await execEsm({
+      file: 'type-definition.ts',
+      project: './tsconfig-baseurl-no-paths.json',
+    });
     expect(err).toBe(null);
   });
 
   test('external modules ignore paths', async () => {
-    const { err } = await exec(
-      `${CMD_ESM_LOADER_WITHOUT_PROJECT} node-modules-import.ts`,
-      {
-        cwd: join(TEST_DIR, './esm-path-mapping'),
-        env: {
-          ...process.env,
-          TS_NODE_PROJECT: `./tsconfig-lodash-path.json`,
-        },
-      }
-    );
+    const { err } = await execEsm({
+      file: 'node-modules-import.ts',
+      project: './tsconfig-lodash-path.json',
+    });
     expect(err).toBe(null);
   });
 
   test('relative imports should ignore paths', async () => {
-    const { stderr, err } = await exec(
-      `${CMD_ESM_LOADER_WITHOUT_PROJECT} path-relative.ts`,
-      {
-        cwd: join(TEST_DIR, './esm-path-mapping'),
-        env: {
-          ...process.env,
-          TS_NODE_PROJECT: `./tsconfig-baseurl-star-path.json`,
-        },
-      }
-    );
+    const { stderr, err } = await execEsm({
+      file: 'path-relative.ts',
+      project: './tsconfig-baseurl-star-path.json',
+    });
+
+    // Expect error & tried candidates to be listed
     expect(err).toBeTruthy();
     expect(stderr).toMatch(`[ERR_MODULE_NOT_FOUND]: Cannot find './1-foo'`);
-    // Expect tried candidates to be listed
     expect(stderr).toMatch(/- file:\/\/.*level-1\/1-foo.ts/);
   });
 
   test('base relative imports should ignore paths', async () => {
-    const { stderr, err } = await exec(
-      `${CMD_ESM_LOADER_WITHOUT_PROJECT} path-base-relative.ts`,
-      {
-        cwd: join(TEST_DIR, './esm-path-mapping'),
-        env: {
-          ...process.env,
-          TS_NODE_PROJECT: `./tsconfig-baseurl-star-path.json`,
-        },
-      }
-    );
+    const { stderr, err } = await execEsm({
+      file: 'path-base-relative.ts',
+      project: './tsconfig-baseurl-star-path.json',
+    });
+
+    // Expect error & tried candidates to be listed
     expect(err).toBeTruthy();
     expect(stderr).toMatch(`[ERR_MODULE_NOT_FOUND]: Cannot find '/1-foo'`);
-    // Expect tried candidates to be listed
     expect(stderr).toMatch(/- file:\/\/.*level-1\/1-foo.ts/);
   });
 });
