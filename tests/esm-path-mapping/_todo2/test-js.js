4e0d363d0b3352060895dba74ed06c43785dab8e
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-03-14 15:12:30 +0700

    temporarily move old tests out of the way

--- tests/esm-path-mapping/_todo2/test-js.js
+++ tests/esm-path-mapping/_todo2/test-js.js
@@ -1,14 +1,14 @@
 import * as assert from 'assert';
 
 // Import js, jsx, ts at baseUrl
-import baseJs from '1-js.js';
-import baseJsx from '2-jsx.js';
-import baseTs from '3-ts.js';
+import baseJs from 'js.js';
+import baseJsx from 'jsx.jsx';
+import baseTs from 'ts.js';
 
 // Import js, jsx, ts under baseUrl
-import childJs from 'child/1-js.js';
-import childJsx from 'child/2-jsx.js';
-import childTs from 'child/3-ts.js';
+import childJs from 'child/js.js';
+import childJsx from 'child/jsx.jsx';
+import childTs from 'child/ts.js';
 
 assert.strictEqual(baseJs, 'base/js');
 assert.strictEqual(baseJsx, 'base/jsx');
