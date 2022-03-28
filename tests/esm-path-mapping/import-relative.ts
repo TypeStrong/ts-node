c5ea9b059e081e069b280080f56d49e759e2fdb9
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-03-29 02:15:58 +0700

    fix import style

--- tests/esm-path-mapping/import-relative.ts
+++ tests/esm-path-mapping/import-relative.ts
@@ -4,12 +4,9 @@ import aboveBaseJsx from '../cjs-path-mapping/level-1/above-base-jsx';
 import aboveBaseTs from '/level-1/above-base-ts';
 
 // Pre-conditions
-const assert = require('assert');
+import * as assert from 'assert';
 
 // Assertions
 assert.strictEqual(aboveBaseJs, 'above-base-js');
 assert.strictEqual(aboveBaseJsx, 'above-base-jsx');
 assert.strictEqual(aboveBaseTs, 'above-base-ts');
-
-// Force this to be a module
-export {};
