4dcff42a6759e451eda624715c7f51688a5f4443
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-03-28 23:12:54 +0700

    destructure import proxyLodash; align both depends-on-lodash deps as cjs

--- tests/cjs-path-mapping/import-within-node-modules.ts
+++ tests/cjs-path-mapping/import-within-node-modules.ts
@@ -1,11 +1,11 @@
 // Should ignore paths when importing inside node_modules
-import dependsOnLodash = require('depends-on-lodash');
+const { proxyLodash } = require('depends-on-lodash');
 
 // Pre-conditions
 const assert = require('assert');
 
 // Assertions
-assert.strictEqual(dependsOnLodash.proxyLodash, 'lodash'); // not our 'lodash-local'
+assert.strictEqual(proxyLodash, 'lodash'); // not our 'lodash-local'
 
 // Force this to be a module
 export {};
--- tests/esm-path-mapping/import-within-node-modules.ts
+++ tests/esm-path-mapping/import-within-node-modules.ts
@@ -1,8 +1,8 @@
 // Should ignore paths when importing inside node_modules
-import dependsOnLodash from 'depends-on-lodash';
+import { proxyLodash } from 'depends-on-lodash';
 
 // Pre-conditions
 import * as assert from 'assert';
 
 // Assertions
-assert.strictEqual(dependsOnLodash.proxyLodash, 'lodash'); // not our 'lodash-local'
+assert.strictEqual(proxyLodash, 'lodash'); // not our 'lodash-local'
