1a93153a081adea5f59ddc0736ca7f6337c6c16e
Andrew Bradley <cspotcode@gmail.com>
2022-06-19 19:29:27 -0400

    WIP adapting to sync resolve; cleaning up esm hook stuff

---
+++ scripts/get-node-nightly.ts
@@ -0,0 +1,25 @@
+#!/usr/bin/env -S ts-node --esm --transpileOnly
+import {existsSync, mkdirSync} from 'fs';
+import {unlinkSync, rmdirSync} from 'fs';
+import {dirname, resolve} from 'path';
+import {$} from '@cspotcode/zx';
+
+async function main() {
+  const __root = dirname(__dirname);
+  const downloadDir = resolve(__root, 'temp/node-nightly');
+  const result = await fetch('https://nodejs.org/download/nightly/index.json');
+  const index = await result.json();
+  const latest = index[0];
+  const {version} = latest;
+  existsSync(downloadDir) && rmdirSync(downloadDir, {recursive: true});
+  mkdirSync(downloadDir, {recursive: true});
+  await $`wget -O ${downloadDir}/download.tar.gz https://nodejs.org/download/nightly/${version}/node-${version}-linux-x64.tar.gz`;
+  process.chdir(downloadDir);
+  await $`tar -xzvf ${downloadDir}/download.tar.gz node-${version}-linux-x64/bin/node`;
+
+  console.log(``);
+  console.log(`export PATH="${downloadDir}/node-${version}-linux-x64/bin:$PATH"`);
+  console.log(``);
+}
+
+main();
