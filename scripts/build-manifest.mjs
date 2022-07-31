29098a1284ccd65740f849e443999bd7a32084c3
Andrew Bradley <cspotcode@gmail.com>
2022-07-31 14:24:18 -0400

    WIP

--- scripts/build-manifest.mjs
+++ scripts/build-manifest.mjs
@@ -1,16 +1,22 @@
-import {readFileSync, writeFileSync} from 'fs';
-import {fileURLToPath} from 'url';
-import {resolve} from 'path';
+import { readFileSync, writeFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { resolve } from 'path';
 
-const manifestPath = resolve(fileURLToPath(import.meta.url), '../../package.json');
+const manifestPath = resolve(
+  fileURLToPath(import.meta.url),
+  '../../package.json'
+);
 const pkg = JSON.parse(readFileSync(manifestPath, 'utf8'));
 
 // Fully splat the "prepare" script so that it does not contain references to a package manager, neither `yarn` nor `npm`
 pkg.scripts.prepare = pkg.scripts.__prepare_template__;
-while(true) {
+while (true) {
   let before = pkg.scripts.prepare;
-  pkg.scripts.prepare = pkg.scripts.prepare.replace(/yarn (\S+)/g, ($0, $1) => pkg.scripts[$1]);
-  if(pkg.scripts.prepare === before) break;
+  pkg.scripts.prepare = pkg.scripts.prepare.replace(
+    /yarn (\S+)/g,
+    ($0, $1) => pkg.scripts[$1]
+  );
+  if (pkg.scripts.prepare === before) break;
 }
 
 writeFileSync(manifestPath, JSON.stringify(pkg, null, 2));
