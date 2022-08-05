a8f1debb9616b82a9691988da09d7bd43fe2a3f3
Andrew Bradley <abradley@brightcove.com>
2022-08-05 21:30:11 +0000

    WIP

--- src/test/ci-node-and-ts-versions.spec.ts
+++ src/test/ci-node-and-ts-versions.spec.ts
@@ -1,6 +1,7 @@
 // When running on CI, double-check that we are testing against the versions of node
 // and typescript in the test matrix.
 
+import { execSync } from 'child_process';
 import semver = require('semver');
 import { ctxTsNode, ts } from './helpers';
 import { context, expect } from './testlib';
@@ -18,9 +19,18 @@ test.suite('Confirm node and typescript versions on CI', (test) => {
     ).toBe(true);
   });
   test('typescript version is correct', async (t) => {
-    expect(process.env.TEST_MATRIX_TYPESCRIPT_VERSION).toBeDefined();
-    expect(
-      semver.satisfies(ts.version, process.env.TEST_MATRIX_TYPESCRIPT_VERSION!)
-    ).toBe(true);
+    let expectedVersion = process.env.TEST_MATRIX_TYPESCRIPT_VERSION!;
+    expect(expectedVersion).toBeDefined();
+    if (expectedVersion === 'next' || expectedVersion === 'latest') {
+      const stdout = execSync(
+        `npm view typescript@${expectedVersion} version --json`,
+        { encoding: 'utf8' }
+      );
+      t.log(stdout);
+      expectedVersion = JSON.parse(stdout);
+      expect(ts.version).toBe(expectedVersion);
+    } else {
+      expect(semver.satisfies(ts.version, expectedVersion)).toBe(true);
+    }
   });
 });
