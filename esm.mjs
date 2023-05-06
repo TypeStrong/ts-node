afbd4b69fc039864c73fd5de60a33eb58fc8d699
isaacs <i@izs.me>
2023-05-05 21:37:04 -0700

    fix: Add globalPreload to ts-node/esm for node 20
    As of node v20, loader hooks are executed in a separate isolated thread
    environment.  As a result, they are unable to register the
    `require.extensions` hooks in a way that would (in other node versions)
    make both CJS and ESM work as expected.
    
    By adding a `globalPreload` method, which *does* execute in the main
    script environment (but with very limited capabilities), these hooks can
    be attached properly, and `--loader=ts-node/esm` will once again make
    both cjs and esm typescript programs work properly.

--- esm.mjs
+++ esm.mjs
@@ -5,3 +5,13 @@ const require = createRequire(fileURLToPath(import.meta.url));
 /** @type {import('./dist/esm')} */
 const esm = require('./dist/esm');
 export const { resolve, load, getFormat, transformSource } = esm.registerAndCreateEsmHooks();
+
+// Affordance for node 20, where load() happens in an isolated thread
+export const globalPreload = () => {
+  const self = fileURLToPath(import.meta.url);
+  return `
+const { createRequire } = getBuiltin('module');
+const require = createRequire(${JSON.stringify(self)});
+require('./dist/esm').registerAndCreateEsmHooks();
+`;
+};
