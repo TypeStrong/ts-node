4d05206d350cb4a5ea7b67ec025cfd950cc7a8ac
Andrew Bradley <cspotcode@gmail.com>
2023-05-07 22:58:02 -0400

    fix git install on windows w/yarn due to spawn issue(?)

--- scripts/prepack.mjs
+++ scripts/prepack.mjs
@@ -1,18 +1,12 @@
-import { spawnSync } from 'child_process';
-const { npm_node_execpath, npm_execpath } = process.env;
-import { readFileSync } from 'fs';
+const { npm_execpath } = process.env;
 
 // prepack is executed by user's package manager when they install from git
 // So cannot assume yarn
 
 if (process.env.TS_NODE_SKIP_PREPACK == null) {
-  if (readFileSync(npm_execpath, 'utf8').match(/^#!.*sh/)) {
-    spawnSync(npm_execpath, ['run', 'prepack-worker'], {
-      stdio: 'inherit',
-    });
-  } else {
-    spawnSync(npm_node_execpath, [npm_execpath, 'run', 'prepack-worker'], {
-      stdio: 'inherit',
-    });
-  }
+  const crossSpawn = require('cross-spawn');
+  const result = crossSpawn.sync(npm_execpath, ['run', 'prepack-worker'], {
+    stdio: 'inherit'
+  });
+  process.exit(result.status);
 }
