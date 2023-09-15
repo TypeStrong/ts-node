e862ed9cc49b3d475aafbff726ea0d195c38eed5
Andrew Bradley <cspotcode@gmail.com>
2023-09-15 19:15:16 -0400

    Fix bug where ts-node would try to use a @tsconfig/bases that is incompatible with the user's very old version of TS

--- src/tsconfigs.ts
+++ src/tsconfigs.ts
@@ -20,17 +20,27 @@ export function getDefaultTsconfigJsonForNodeVersion(ts: TSCommon): any {
     const config = require('@tsconfig/node16/tsconfig.json');
     if (configCompatible(config)) return config;
   }
-  return require('@tsconfig/node14/tsconfig.json');
+  {
+    const config = require('@tsconfig/node14/tsconfig.json');
+    if (configCompatible(config)) return config;
+  }
+  // Old TypeScript compilers may be incompatible with *all* @tsconfig/node* configs,
+  // so fallback to nothing
+  return {};
 
   // Verify that tsconfig target and lib options are compatible with TypeScript compiler
   function configCompatible(config: {
     compilerOptions: {
       lib: string[];
       target: string;
+      module: string;
+      moduleResolution: string;
     };
   }) {
     return (
       typeof (ts.ScriptTarget as any)[config.compilerOptions.target.toUpperCase()] === 'number' &&
+      typeof (ts.ModuleKind as any)[config.compilerOptions.module.toUpperCase()] === 'number' &&
+      typeof (ts.ModuleResolutionKind as any)[config.compilerOptions.moduleResolution.toUpperCase()] === 'number' &&
       tsInternal.libs &&
       config.compilerOptions.lib.every((lib) => tsInternal.libs!.includes(lib))
     );
