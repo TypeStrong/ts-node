5882c17b757fd9774d6989637fa6f965d10733be
calebboyd <caleb.boyd@gmail.com>
2024-02-22 15:47:40 -0600

    feat(esm): initialize hook - register(_,[_,tsNodeOptions])

--- esm.mjs
+++ esm.mjs
@@ -4,4 +4,30 @@ const require = createRequire(fileURLToPath(import.meta.url));
 
 /** @type {import('./dist/esm')} */
 const esm = require('./dist/esm');
-export const { resolve, load, getFormat, transformSource } = esm.registerAndCreateEsmHooks();
+
+/** @type {ReturnType<typeof esm['registerAndCreateEsmHooks']> | undefined} */
+let loader = undefined;
+
+export function resolve(...args) {
+  if (!loader) initialize();
+  return loader.resolve.apply(this, args);
+}
+
+export function load(...args) {
+  if (!loader) initialize();
+  return loader.load.apply(this, args);
+}
+
+export function getFormat(...args) {
+  if (!loader) initialize();
+  return loader.getFormat.apply(this, args);
+}
+
+export function transformSource(...args) {
+  if (!loader) initialize();
+  return loader.transformSource.apply(this, args);
+}
+
+export function initialize(tsNodeOptions) {
+  loader = esm.registerAndCreateEsmHooks(tsNodeOptions);
+}
